75488d801e57feaca82670f4a933ff91efe15f75
Were Samson Bruno <werefast2000@gmail.com>
2023-02-09 12:18:16 +0300

    graphql config

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "digitalocean": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
